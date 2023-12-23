e557f029b3f9238a7dcf77e3d2ce7dafa7570241
Abbas Superstar <111567007+abbastahir5862@users.noreply.github.com>
2023-12-23 14:22:20 +0300

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -56,6 +56,7 @@ const Contact = () => {
                                 <Form></Form>
                             </div>
                             <div className="contact__area-bottom-map">
+                                {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830894606!2d-74.11976383964463!3d40.69766374865767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1652012644726!5m2!1sen!2sbd" loading="lazy"></iframe>
                             </div>
                         </div>
