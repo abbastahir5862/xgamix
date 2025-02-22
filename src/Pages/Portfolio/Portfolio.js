477b6cfb4ca171b69981082aca4ab4c61c9ce6e4
Abbas Tahir <111567007+abbastahir5862@users.noreply.github.com>
2025-02-22 14:18:36 +0500

    Update Portfolio.js

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import FooterThree from '../Footer/FooterThree';
-import Header from '../Header/HeaderFour';
+import Header from '../Header/HeaderTwo';
 import { pageTitle } from '../PageTitle';
 
 const Portfolio = () => {
